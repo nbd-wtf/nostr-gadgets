85a0bc93f6b8b4fa99548e19c6db54a38cc7adc7
fiatjaf <fiatjaf@gmail.com>
2025-01-13 23:58:52 -0300

    global: setPool()

--- global.ts
+++ global.ts
@@ -4,7 +4,14 @@ import { MemoryHints } from './hints'
 /**
  * pool is a global used by all other functions in this library. Feel free to use it directly in your app.
  */
-export const pool: SimplePool = new SimplePool()
+export let pool: SimplePool = new SimplePool()
+
+/**
+ * setPool sets the global pool -- do not use unless you know what you're doing.
+ */
+export function setPool(p: SimplePool) {
+  pool = p
+}
 
 /**
  * hints is a global used by other functions in this library. Use it directly.
