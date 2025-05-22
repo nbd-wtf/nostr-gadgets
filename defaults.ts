094f4da0c0d900066145e6d7c6d5cbf5e94cd43e
fiatjaf <fiatjaf@gmail.com>
2025-05-22 17:53:05 -0300

    update default relay lists.

--- defaults.ts
+++ defaults.ts
@@ -1,8 +1,8 @@
-export const METADATA_QUERY_RELAYS = ['wss://purplepag.es', 'wss://relay.nos.social', 'wss://user.kindpag.es']
+export const METADATA_QUERY_RELAYS = ['wss://purplepag.es', 'wss://relay.nos.social', 'wss://relay.primal.net']
 
-export const RELAYLIST_RELAYS = ['wss://purplepag.es', 'wss://relay.nos.social', 'wss://user.kindpag.es']
+export const RELAYLIST_RELAYS = ['wss://purplepag.es', 'wss://relay.nos.social', 'wss://indexer.coracle.social']
 
-export const ARBITRARY_IDS_RELAYS = ['wss://cache2.primal.net/v1', 'wss://relay.noswhere.com', 'wss://relay.nostr.band']
+export const ARBITRARY_IDS_RELAYS = ['wss://cache2.primal.net/v1', 'wss://relay.damus.io', 'wss://relay.nostr.band']
 
 export const SEARCH_RELAYS = [
   'wss://relay.nostr.band',
